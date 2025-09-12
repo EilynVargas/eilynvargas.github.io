0a7de046e5b87c3e41158eb6fceea528db787f4e
Eilyn-Vargas <eilyn.vargas-almonte@viseo.com>
2025-09-12 18:53:15 -0400

    Creating loader of the site

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
+// Object with duration/delay values
+const CONFIG = {
+    TRANSITION_DELAY: 1000,
+    COUNTER_DELAY: 3000,
+    COUNTER_INTERVAL: 30
+}
+
+// Counter function that starts after a specified delay and runs at a set interval.
+function startCounter(){
+    setTimeout(function timeoutCounter(){
+        const counterNumber = document.getElementById('counter');
+        const loaderContainer = document.getElementById('loader');
+        let counter = 0;
+
+        const myInterval = setInterval(progress, CONFIG.COUNTER_INTERVAL);
+        function progress(){
+            //Counter stops at 100%
+            if(counter==100){
+                clearInterval(myInterval);
+                // When counter reach 100, the loader disappears from the screen and displays hero-banner
+                setTimeout(function timeoutLoaderContainer(){
+                    loaderContainer.style.transform = "translate(0,-100vh)";
+                }, CONFIG.TRANSITION_DELAY)
+            }
+            else{
+                counter++;
+                counterNumber.textContent = counter + "%";
+            }
+        }
+    }, CONFIG.COUNTER_DELAY);
+}
+
+// Function that adds a class to the HTML element, where shows a translation of the logo when loader starts
+function preloadLogo(){
+    const preloadElement = document.getElementById('preload-logo');
+    preloadElement.classList.add('logo-loaded');
+}
+
 // Code for loader
 window.addEventListener('load', function() {
-    const counterNumber = document.getElementById('counter');
-    let counter = 0;
-    const myInterval = setInterval(progress, 30);
-    function progress(){
-        if(counter==100){
-            clearInterval(myInterval);
-        }
-        else{
-            counter++;
-            counterNumber.textContent = counter + "%";
-        }
-    }
+    setTimeout(preloadLogo, CONFIG.TRANSITION_DELAY);
+    startCounter();
 });
+
 
 // Code for scroll animation
 window.addEventListener('scroll', function() {
